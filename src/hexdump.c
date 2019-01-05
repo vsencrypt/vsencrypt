5525c21e331e5be4f5f49bde56f416239d6ee221
Ming <mingchen@users.noreply.github.com>
2019-01-04 20:08:10 -0800

    donot use c99 for

--- src/hexdump.c
+++ src/hexdump.c
@@ -5,7 +5,9 @@ const char *hexdump(const u_int8_t *buf,
                     size_t buf_nbytes,
                     char *hex_out)
 {
-    for (int i = 0; i < buf_nbytes; ++i)
+    int i;
+
+    for (i = 0; i < buf_nbytes; ++i)
     {
         sprintf((char *)(hex_out + i * 2), "%02x", buf[i]);
     }
