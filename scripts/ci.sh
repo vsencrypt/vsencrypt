4a426e9736fed9e3ddef7f45492525d4f9e4e872
Ming <mingchen@users.noreply.github.com>
2019-01-10 07:21:08 -0800

    travis-ci: try to find msbuild.exe and cl.exe

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 if [[ $TRAVIS_OS_NAME == 'windows' ]]; then
+  find / -name "MsBuild.exe"
+  find / -name "cl.exe"
   msbuild /target:Build /p:Configuration=Debug   /p:Platform=Win32
   msbuild /target:Build /p:Configuration=Release /p:Platform=Win32
   msbuild /target:Build /p:Configuration=Debug   /p:Platform=x64
