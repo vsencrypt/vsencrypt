a8df9e533159c49a5405fae9b0b4f46f7bc8b860
Ming <mingchen@users.noreply.github.com>
2019-01-09 20:38:24 -0800

    travis-ci: Support windows

---
+++ scripts/ci.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ $TRAVIS_OS_NAME == 'windows' ]]; then
+  msbuild /target:Build /p:Configuration=Debug   /p:Platform=Win32
+  msbuild /target:Build /p:Configuration=Release /p:Platform=Win32
+  msbuild /target:Build /p:Configuration=Debug   /p:Platform=x64
+  msbuild /target:Build /p:Configuration=Release /p:Platform=x64
+else
+  # Linux or mac
+  make
+  make test
+fi
