df1c299566362802166631223d9c4f3b3693f21c
Ming <mingchen@users.noreply.github.com>
2022-09-22 09:53:36 -0700

    Github action

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -8,8 +8,13 @@ if [[ $RUNNER_OS == 'Windows' ]]; then
   msbuild /target:Build /p:Configuration=Release /p:Platform=Win32
   msbuild /target:Build /p:Configuration=Debug   /p:Platform=x64
   msbuild /target:Build /p:Configuration=Release /p:Platform=x64
+  find .
+  uname -s
 else
   # Linux or mac
   make
   make test
+  out=vsencrypt.$(uname -s)
+  mv vsencrypt $out
+  find .
 fi
